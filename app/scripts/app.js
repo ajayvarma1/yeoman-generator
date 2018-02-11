1cd7f76375a107a07f86558e64e979d97f9a6b08
ajayvarma <ajayvarma3373@gmail.com>
2018-02-11 15:37:13 +0530

    Frist commit

---
+++ app/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myApp
+ * @description
+ * # myApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider,$locationProvider) {
+    $locationProvider.hashPrefix("");
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myApp
+ */
+angular.module('myApp')
+  .controller('AboutCtrl', function () {
+
+  });
